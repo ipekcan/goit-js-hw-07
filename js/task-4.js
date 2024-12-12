99a1310059ba72833c24f948df79bcf12006af00
ipekcan <ipekcan@hotmail.com>
2024-12-12 22:54:21 +0300

    task-4 completed

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+"use strict";
+const form = document.querySelector(".login-form");
+const user = [];
+form.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  } else {
+    user.push(email.trim());
+    user.push(password.trim());
+  }
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();
+}
