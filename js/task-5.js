724f700e41cfe87de88a2b02dc24af5c2b604a7a
ipekcan <ipekcan@hotmail.com>
2024-12-14 01:30:15 +0300

    task-5 corrected

--- js/task-5.js
+++ js/task-5.js
@@ -1,13 +1,20 @@
-const btnColor = document.querySelector(".change-color");
-const color = document.querySelector(".color");
-color.textContent = btnColor.addEventListener("click",getRandomHexColor);
+const btnColor = document.querySelector("button");
+const color = document.querySelector("span");
+const rndColor = btnColor.addEventListener("click",getRandomHexColor);
 
 function getRandomHexColor() {
-  const rndNum = Math.floor(Math.random() * 16777215)
-  .toString(16)
-  .padStart(6, 0);
-  color.textContent = rndNum;
-  document.body.style.backgroundColor = `#${color.textContent}`;
-  return `#${rndNum}`;
-}
+ const rndNum = Math.floor(Math.random() * 16777215)
+.toString(16)
+.padStart(6, 0);
+color.textContent = `#${rndNum}`;
+document.body.style.backgroundColor = `${color.textContent}`;
+return `#${rndNum}`;
+} 
+// function getRandomHexColor() {
+//   color.textContent = rndColor;
+//   document.body.style.backgroundColor = rndColor;
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
 
