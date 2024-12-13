aa580d393ed22bf49f38ba3b0684f3fc75247cff
ipekcan <ipekcan@hotmail.com>
2024-12-14 01:00:31 +0300

    task-3 corrected

--- js/task-3.js
+++ js/task-3.js
@@ -2,10 +2,22 @@
 const elemTxtBox = document.querySelector("#name-input");
 let name = "";
 let logName = document.querySelector("#name-output");
-const newName = elemTxtBox.addEventListener("keyup",writeName);
+const newName = elemTxtBox.addEventListener("keyup", writeName);
 function writeName(e) {
-    elemTxtBox.textContent += `${e.key}`;
-    name = elemTxtBox.textContent.trim();
-    if(name !== "")
-        logName.innerHTML = name;
+    if (e.keyCode>64 && e.keyCode<91) {
+        elemTxtBox.textContent += `${e.key}`;
+        name = elemTxtBox.textContent.trim();
+        if (name === "") 
+            logName.innerHTML = "Anonymous";
+        back();
+    } else if (e.keyCode == 8) {
+        back();
+    }
+}
+function back() {
+    if(elemTxtBox.value.trim() !== "")
+        logName.innerHTML = elemTxtBox.value;
+    else {
+        logName.innerHTML = "Anonymous";
+    }
 }
