0f7f6e951d55e154a13d4fb4f3a4ec7e58b835b4
ipekcan <ipekcan@hotmail.com>
2024-12-15 15:34:49 +0300

    task-3 corrected

--- js/task-3.js
+++ js/task-3.js
@@ -1,23 +1,7 @@
 "use strict";
 const elemTxtBox = document.querySelector("#name-input");
-let name = "";
-let logName = document.querySelector("#name-output");
-const newName = elemTxtBox.addEventListener("keyup", writeName);
-function writeName(e) {
-    if (e.keyCode>64 && e.keyCode<91) {
-        elemTxtBox.textContent += `${e.key}`;
-        name = elemTxtBox.textContent.trim();
-        if (name === "") 
-            logName.innerHTML = "Anonymous";
-        back();
-    } else if (e.keyCode == 8) {
-        back();
-    }
-}
-function back() {
-    if(elemTxtBox.value.trim() !== "")
-        logName.innerHTML = elemTxtBox.value;
-    else {
-        logName.innerHTML = "Anonymous";
-    }
-}
+const logName = document.querySelector("#name-output");
+
+elemTxtBox.addEventListener("input",()=>{
+    elemTxtBox.value.trim()!==""?logName.textContent = elemTxtBox.value.trim() : logName.textContent = "Anonymous";
+})
